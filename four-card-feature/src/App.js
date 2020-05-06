27fa561aaf7136218748439c03f6a7314d3704ad
Muhammad A.G. Bello <magbello157@gmail.com>
2020-05-06 11:44:53 +0100

    add card component and section

--- four-card-feature/src/App.js
+++ four-card-feature/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import PageHead from './components/PageHead';
+import Card from './components/Card';
 
 class App extends Component {
   render() {
     return (
       <div>
         <PageHead />
+        <div className="card-section">
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ four-card-feature/src/components/Card.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import image from '../images/icon-karma.svg';
+
+const Box = styled.div`
+  background: #fff;
+  border: 1px solid #ccc;
+  padding: 1.5rem;
+  margin: 1rem;
+  margin-top: 2;
+  width: 20rem;
+  height: 15rem;
+  border-radius: 0.4rem;
+  box-shadow: 2px 6px 10px 0px rgba(82,75,82,0.53);
+`;
+
+const Title = styled.h3`
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: hsl(234, 12%, 34%);
+  text-align: left;
+  padding-left: 10px;
+`;
+
+const Body = styled.p`
+  color: hsl(229, 6%, 66%);
+  font-size: 15px;
+  font-weight: 200;
+  padding: 10px;
+  text-align: left;
+`;
+
+const Icon = styled.img`
+  float: right;
+  margin-right: 5px;
+`;
+
+class Card extends Component {
+
+  static defaultProps = {
+    title: 'Box',
+    body: 'Lorem ipsum dolor sit amet',
+    image: image
+  }
+
+  render() {
+    return (
+      <Box>
+        <Title>{this.props.title}</Title>
+        <Body>{this.props.body}</Body>
+        <Icon src={this.props.image} alt={this.props.alt} />
+      </Box>
+    );
+  }
+}
+
+export default Card;
